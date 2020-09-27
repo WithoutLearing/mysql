63c29765e8bffb52664da38024329256d4682103
WithoutLearing <cuifei2017@hotmail.com>
2020-09-27 22:26:13 +0800

    创建mysql

--- mysql/Form1.Designer.cs
+++ mysql/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_CreateDB = new System.Windows.Forms.Button();
+            this.button_DropDB = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_CreateDB
+            // 
+            this.button_CreateDB.Location = new System.Drawing.Point(610, 49);
+            this.button_CreateDB.Name = "button_CreateDB";
+            this.button_CreateDB.Size = new System.Drawing.Size(96, 43);
+            this.button_CreateDB.TabIndex = 0;
+            this.button_CreateDB.Text = "创建数据库";
+            this.button_CreateDB.UseVisualStyleBackColor = true;
+            this.button_CreateDB.Click += new System.EventHandler(this.button_CreateDB_Click);
+            // 
+            // button_DropDB
+            // 
+            this.button_DropDB.Location = new System.Drawing.Point(610, 113);
+            this.button_DropDB.Name = "button_DropDB";
+            this.button_DropDB.Size = new System.Drawing.Size(96, 43);
+            this.button_DropDB.TabIndex = 1;
+            this.button_DropDB.Text = "删除数据库";
+            this.button_DropDB.UseVisualStyleBackColor = true;
+            this.button_DropDB.Click += new System.EventHandler(this.button_DropDB_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_DropDB);
+            this.Controls.Add(this.button_CreateDB);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_CreateDB;
+        private System.Windows.Forms.Button button_DropDB;
     }
 }
 
--- mysql/Form1.cs
+++ mysql/Form1.cs
@@ -7,14 +7,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;//调用MySQL动态库
 
 namespace mysql
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// 创建连接mysql
+        /// </summary>
+        public void ConnectMysql()
+        {
+            string connstr = "Server = localhost; UserId = root; Password = 19891018; Database = dbdb";//数据库登录信息，其中localhost代表本地计算机
+            MySqlConnection mySqlConnection = new MySqlConnection(connstr);
+            mySqlConnection.Open();
+        }
+
+        /// <summary>
+        /// 创建数据库
+        /// </summary>
+        /// <param name="DBname">数据库名称</param>
+        public void CreateDB(string name)
+        {
+            string DBname=string.Format("CREATE DATABASE {0}", name);
+            MySqlConnection conn = new MySqlConnection("Data Source=localhost;Persist Security Info=yes;UserId=root; PWD=19891018;");//数据库登录信息，其中localhost代表本地计算机
+            MySqlCommand cmd = new MySqlCommand(DBname, conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        /// <summary>
+        /// 删除数据库
+        /// </summary>
+        /// <param name="DBname">数据库名称</param>
+        public void DropDB(string name)
+        {
+            string DBname = string.Format("drop database {0}", name);
+            MySqlConnection conn = new MySqlConnection("Data Source=localhost;Persist Security Info=yes;UserId=root; PWD=19891018;");//数据库登录信息，其中localhost代表本地计算机
+            MySqlCommand cmd = new MySqlCommand(DBname, conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_CreateDB_Click(object sender, EventArgs e)
+        {
+            CreateDB("test");
+        }
+
+        private void button_DropDB_Click(object sender, EventArgs e)
+        {
+            DropDB("dbdb");
         }
     }
 }
