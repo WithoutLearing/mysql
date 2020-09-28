f56445e839fcafe64d110869a06f72703c62f43d
fqy <cuifei2017@hotmail.com>
2020-09-28 17:24:33 +0800

    建表

--- mysql/Form1.cs
+++ mysql/Form1.cs
@@ -11,51 +11,36 @@ using MySql.Data.MySqlClient;//调用MySQL动态库
 
 namespace mysql
 {
+    /// <summary>
+    /// 考核基础状态表
+    /// </summary>
+    struct AssessmentBasisStatusTableStruct
+    {
+        public int Uuid; //考核唯一标识
+        public string KHKM;//考核科目
+        public string DWXX;//考核地点
+        public short GHDTSL;//规划电台数量
+        public double KHSJ;//考核开始时间
+        public double JSHJ;//考核结束时间
+        public short PBDN;//平板电脑
+        public short ZHBC;//作战编成
+        public int CLID;//车辆ID
+        public string KHMD;//预留
+        public string KHMS;//预留
+    }
+
+
     public partial class Form1 : Form
     {
+        mysql Mysql = new mysql();
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        /// <summary>
-        /// 创建连接mysql
-        /// </summary>
-        public void ConnectMysql()
-        {
-            string connstr = "Server = localhost; UserId = root; Password = 19891018; Database = dbdb";//数据库登录信息，其中localhost代表本地计算机
-            MySqlConnection mySqlConnection = new MySqlConnection(connstr);
-            mySqlConnection.Open();
-        }
+   
 
-        /// <summary>
-        /// 创建数据库
-        /// </summary>
-        /// <param name="DBname">数据库名称</param>
-        public void CreateDB(string name)
-        {
-            string DBname=string.Format("CREATE DATABASE {0}", name);
-            MySqlConnection conn = new MySqlConnection("Data Source=localhost;Persist Security Info=yes;UserId=root; PWD=19891018;");//数据库登录信息，其中localhost代表本地计算机
-            MySqlCommand cmd = new MySqlCommand(DBname, conn);
-            conn.Open();
-            cmd.ExecuteNonQuery();
-            conn.Close();
-        }
-
-        /// <summary>
-        /// 删除数据库
-        /// </summary>
-        /// <param name="DBname">数据库名称</param>
-        public void DropDB(string name)
-        {
-            string DBname = string.Format("drop database {0}", name);
-            MySqlConnection conn = new MySqlConnection("Data Source=localhost;Persist Security Info=yes;UserId=root; PWD=19891018;");//数据库登录信息，其中localhost代表本地计算机
-            MySqlCommand cmd = new MySqlCommand(DBname, conn);
-            conn.Open();
-            cmd.ExecuteNonQuery();
-            conn.Close();
-        }
 
 
         private void Form1_Load(object sender, EventArgs e)
@@ -65,12 +50,27 @@ namespace mysql
 
         private void button_CreateDB_Click(object sender, EventArgs e)
         {
-            CreateDB("test");
+            Mysql.CreateDB("dbdb");
         }
 
         private void button_DropDB_Click(object sender, EventArgs e)
         {
-            DropDB("dbdb");
+            Mysql.DropDB("dbdb");
+        }
+
+        private void button_CreateTable_Click(object sender, EventArgs e)
+        {
+            Mysql.CreateTable("dbdb", "Test (Field1 VarChar(50), Field2 Integer)");
+        }
+
+        private void button_DropTable_Click(object sender, EventArgs e)
+        {
+            Mysql.DropTable("dbdb", "Test");
+        }
+
+        private void button_OpenDB_Click(object sender, EventArgs e)
+        {
+            Mysql.ConnectMysql("db");
         }
     }
 }
---
+++ mysql/mysql.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;//调用MySQL动态库
+
+namespace mysql
+{
+    /// <summary>
+    /// 关于SQL操作的类
+    /// </summary>
+    public class mysql
+    {
+        /// <summary>
+        /// 考核基础状态表字段
+        /// </summary>
+        public string AssessmentBasisStatusTable = "AssessmentBasisStatusTable (Uuid INT , KHKM VARCHAR(50))";
+
+
+        /// <summary>
+        /// 连接一个已经存在的数据库,并打开
+        /// </summary>
+        /// <param name="name">已存在的数据库名称</param>
+        public void ConnectMysql(string name)
+        {
+            try
+            {
+                string connstr = string.Format("Server = localhost; UserId = root; Password = 19891018; Database = {0}", name);//数据库登录信息，其中localhost代表本地计算机
+                MySqlConnection mySqlConnection = new MySqlConnection(connstr);
+                mySqlConnection.Open();
+            }
+            catch 
+            {
+
+               
+
+            }
+            
+        }
+
+        /// <summary>
+        /// 连接一个已经存在的数据库,并关闭
+        /// </summary>
+        /// <param name="name">已存在的数据库名称</param>
+        public void DisconnectMysql(string name)
+        {
+            try
+            {
+                string connstr = string.Format("Server = localhost; UserId = root; Password = 19891018; Database = {0}", name);//数据库登录信息，其中localhost代表本地计算机
+                MySqlConnection mySqlConnection = new MySqlConnection(connstr);
+                mySqlConnection.Close();
+            }
+            catch
+            {
+
+
+
+            }
+        }
+
+        /// <summary>
+        /// 创建数据库
+        /// </summary>
+        /// <param name="DBname">数据库名称</param>
+        public void CreateDB(string name)
+        {
+            try
+            {
+                string DBname = string.Format("CREATE DATABASE {0}", name);
+                MySqlConnection conn = new MySqlConnection("Data Source=localhost;Persist Security Info=yes;UserId=root; PWD=19891018;");//数据库登录信息，其中localhost代表本地计算机
+                MySqlCommand cmd = new MySqlCommand(DBname, conn);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch
+            {
+
+
+            }
+
+        }
+
+        /// <summary>
+        /// 删除数据库
+        /// </summary>
+        /// <param name="DBname">数据库名称</param>
+        public void DropDB(string name)
+        {
+            try
+            {
+                string DBname = string.Format("drop database {0}", name);
+                MySqlConnection conn = new MySqlConnection("Data Source=localhost;Persist Security Info=yes;UserId=root; PWD=19891018;");//数据库登录信息，其中localhost代表本地计算机
+                MySqlCommand cmd = new MySqlCommand(DBname, conn);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch
+            {
+
+
+            }
+
+        }
+
+        /// <summary>
+        /// 创建数据表
+        /// </summary>
+        /// <param name="DBname">数据库名称</param>
+        /// <param name="name">新建表名称</param>
+        public void CreateTable(string DBname, string name)
+        {
+            try
+            {
+                string Tablename = string.Format("CREATE TABLE IF NOT EXISTS {0}", name);//不存在创建，已存在不创建
+                string connstr = string.Format("Server = localhost; UserId = root; Password = 19891018; Database = {0}", DBname);//数据库登录信息，其中localhost代表本地计算机
+                MySqlConnection conn = new MySqlConnection(connstr);
+                MySqlCommand cmd = new MySqlCommand(Tablename, conn);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch
+            {
+
+
+            }
+        }
+
+        /// <summary>
+        /// 删除数据表
+        /// </summary>
+        /// <param name="DBname">数据库名称</param>
+        /// <param name="name">要删除的表名称</param>
+        public void DropTable(string DBname, string name)
+        {
+            string Tablename = string.Format("DROP TABLE IF EXISTS {0}", name);
+            string connstr = string.Format("Server = localhost; UserId = root; Password = 19891018; Database = {0}", DBname);//数据库登录信息，其中localhost代表本地计算机
+            MySqlConnection conn = new MySqlConnection(connstr);
+            MySqlCommand cmd = new MySqlCommand(Tablename, conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+
+
+
+
+    }
+}
